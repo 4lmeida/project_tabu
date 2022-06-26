5d518156c118a2f8661d0067450a31bdbc9ac612
Luis Almeida <lferreira646@gmail.com>
2022-06-26 14:16:17 -0300

    construtor e atributos

--- src/entities/Calculation.java
+++ src/entities/Calculation.java
@@ -1,5 +1,19 @@
 package entities;
 
 public class Calculation {
+	
+	private Integer a;
+	private Integer b;
+	private Integer c;
+	
+	public Calculation() {
+	}
 
+	public Calculation(Integer a, Integer b, Integer c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	
 }
